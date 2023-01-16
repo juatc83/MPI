dae5765a5b7fe99ba6cc8974db47d762a6ad5734
Ju <julien.nugue3@gmail.com>
2023-01-16 12:57:52 +0100

    modified:   arch/make_inc new file:   idrismpi.pdf new file:   idrismpiaidefortran.pdf new file:   idrismpitp.pdf new file:   tp3/a.out new file:   tp3/pi modified:   tp3/pi.f90 new file:   tp3/pi.o 	deleted:    idrismpi.pdf 	deleted:    idrismpiaidefortran.pdf 	deleted:    idrismpitp.pdf 	deleted:    tp3/a.out 	deleted:    tp3/pi 	modified:   tp3/pi.f90 	deleted:    tp3/pi.o

--- tp3/pi.f90
+++ tp3/pi.f90
@@ -1,23 +1,38 @@
-program pi
+program reduce
+  use mpi
   implicit none
+  integer :: nb_procs,rang,code
   integer, parameter :: dp = kind(1.d0)
   integer, parameter :: li = selected_int_kind(15)
   integer(kind=li):: nbbloc,i
-  real(kind=dp) :: largeur,somme,x
+  real(kind=dp) :: largeur,somme,valeur,x,longueur_tranche,pi
+
+  call MPI_INIT (code)
+  call MPI_COMM_SIZE ( MPI_COMM_WORLD ,nb_procs,code)
+  call MPI_COMM_RANK ( MPI_COMM_WORLD ,rang,code)
 
   ! Nombre d'intervalles
   nbbloc = 3*1000*1000_li*100
   ! largeur des intervalles
   largeur = 1._dp / real(nbbloc,dp)
 
+  longueur_tranche = nbbloc/real(nbbloc,dp)
+
   somme = 0._dp
 
-  do i=1, nbbloc
+  do i = longueur_tranche*(rang)+1, longueur_tranche*(rang+1)
     ! Point au milieu de l'intervalle
     x = largeur*(i-0.5_dp)
     ! Calcul de l'aire
     somme = somme + largeur*(4._dp / (1._dp + x*x))
   end do
 
-  print *, "Pi =", somme
-end program
+
+  call MPI_REDUCE (somme,pi,1, MPI_DOUBLE_PRECISION , MPI_SUM ,0, MPI_COMM_WORLD,code)
+
+  if(rang==0)then
+    print *, "Pi =", pi  
+  endif
+
+  call MPI_FINALIZE (code)
+  end program reduce
